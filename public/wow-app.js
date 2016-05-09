82a3525ab42156a424f8a6760192d6a95dc5c719
David Gurney <gurneyds@ldschurch.org>
2016-05-09 08:09:29 -0600

    Styling changes

--- public/wow-app.js
+++ public/wow-app.js
@@ -6,22 +6,26 @@
 		var control = this;
 
 		control.orgInfo = {
-		      orgName: 'Bishopric',
-		      callings: [
-		        {
-		          name: 'Bishop',
-		          positions: 1
-		        },
-		        {
-		          name: '1st Councilor',
-		          positions: 1
-		        },
-		        {
-		          name: '2nd Councilor',
-		          positions: 1
-		        }
-		      ]
-		  };
+			orgName: 'Bishopric',
+			callings: [
+				{
+					name: 'Bishop',
+					positions: 1
+				},
+				{
+					name: '1st Councilor',
+					positions: 1
+				},
+				{
+					name: '2nd Councilor',
+					positions: 1
+				},
+				{
+					name: 'Exec Secretary',
+					positions: 1
+				}
+			]
+		};
 
 
 	}]);
